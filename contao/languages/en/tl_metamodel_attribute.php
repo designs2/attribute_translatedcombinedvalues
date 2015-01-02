2dc6b663ec8053d99dab18591046bd392b850594
David Greminger <david.greminger@1up.io>
2015-01-02 17:19:25 +0100

    Fixed author-validation

--- contao/languages/en/tl_metamodel_attribute.php
+++ contao/languages/en/tl_metamodel_attribute.php
@@ -9,8 +9,8 @@
  * PHP version 5
  * @package     MetaModels
  * @subpackage  AttributeCombinedValues
- * @author      Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @author      Stefan Heimes <cms@men-at-work.de>
+ * @author      Andreas Isaak <andy.jared@googlemail.com>
+ * @author      David Greminger <david.greminger@1up.io>
  * @copyright   The MetaModels team.
  * @license     LGPL.
  * @filesource
--- src/MetaModels/DcGeneral/Events/Table/Attribute/Translated/CombinedValues/PropertyAttribute.php
+++ src/MetaModels/DcGeneral/Events/Table/Attribute/Translated/CombinedValues/PropertyAttribute.php
@@ -8,7 +8,8 @@
  * PHP version 5
  * @package    MetaModels
  * @subpackage Core
- * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author      Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author      David Greminger <david.greminger@1up.io>
  * @copyright  The MetaModels team.
  * @license    LGPL.
  * @filesource
