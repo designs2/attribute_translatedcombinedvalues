30ce8ddeeb457b20f6d592a25d334b41c736089d
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-01-10 03:23:58 +0100

    Code cleanup.

--- contao/config/autoload.php
+++ contao/config/autoload.php
@@ -7,13 +7,14 @@
  * data in each collection.
  *
  * PHP version 5
- * @package     MetaModels
- * @subpackage  AttributeCombinedValues
- * @author      Stefan Heimes <stefan_heimes@hotmail.com>
- * @author      Andreas Isaak <andy.jared@googlemail.com>
- * @author      David Greminger <david.greminger@1up.io>
- * @copyright   The MetaModels team.
- * @license     LGPL.
+ * @package    MetaModels
+ * @subpackage TranslatedCombinedValues
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Stefan Heimes <stefan_heimes@hotmail.com>
+ * @author     Andreas Isaak <andy.jared@googlemail.com>
+ * @author     David Greminger <david.greminger@1up.io>
+ * @copyright  The MetaModels team.
+ * @license    LGPL.
  * @filesource
  */
 
